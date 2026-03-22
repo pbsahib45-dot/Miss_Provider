c1b1283922577d260b2cf5400b1b34aa8942cb1c
pbsahib45-dot <pbsahib45@gmail.com>
2026-03-22 12:53:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,7 +120,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "7200"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/filmygraph4') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/filmygram47') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Jox_444') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/updatefilmx') # Update channel link for the bot
 
