5a619424ffe29eb4c6515a5e3e337550cf1760dc
pbsahib45-dot <pbsahib45@gmail.com>
2026-03-26 13:28:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,3 +99,18 @@ if __name__ == '__main__':
         except KeyboardInterrupt:
             logging.info('Service Stopped Bye 👋')
             break
+
+import os
+from aiohttp import web
+import threading
+
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+def run_web():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    port = int(os.environ.get("PORT", 10000))
+    web.run_app(app, host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
